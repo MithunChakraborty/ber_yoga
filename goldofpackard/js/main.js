8cc5eb81b15611dc31cda9fbeed19028bcd14b30
mithun <motyletsdenis@gmail.com>
2020-03-24 23:08:41 +0700

    3-conti

--- goldofpackard/js/main.js
+++ goldofpackard/js/main.js
@@ -6,9 +6,19 @@ const menuBtn = document.getElementById("mobile_enter");
 const menuCloseBtn = document.getElementById("mobile_exit");
 const menuBtn2 = document.getElementById("mobile_enter2");
 const mobile_logo_block = document.getElementById("mobile_logo_block");
+var height_header;
+var margin_section;
+const height_orient1 = "7.7rem";
+const margin_orient1 = "7.7rem 0px 0px";
+const height_orient2 = "10rem";
+const margin_orient2 = "10rem 0px 0px";
+const third_btn = document.getElementById("third_btn");
+const third_sec2 = document.getElementById("third_section_continue");
+const third_sign = document.getElementById("third_sign");
 
 window.onload = function() {
     //
+    third_sec2.style.marginTop = `-${third_sec2.offsetHeight}px`;
     //
     if (this.document.body.offsetWidth < 929) {
         _nav.style.display = "flex";
@@ -18,8 +28,11 @@ window.onload = function() {
         document.body.style.overflow = 'auto';
         _header.style.height = `${mobile_logo_block.offsetHeight}px`;
         first_section.style.margin = `${_header.offsetHeight}px 0px 0px`;
-        _header.style.top = 0 
-
+        _header.style.top = 0
+        //
+        this.height_header = _header.style.height;
+        this.margin_section = first_section.style.margin;
+        //
         var list = _nav.children;
         for (var j=0, child; child=list[j]; j++) {
             child.addEventListener('click', () => {
@@ -31,14 +44,16 @@ window.onload = function() {
                     _header.style.height = `${mobile_logo_block.offsetHeight}px`;
                     i--;
                 }
-                else {
-                    
+                else {                   
                     i++;
                 }
             })
         }
     }
 };
+window.addEventListener("resize", function() {
+    third_sec2.style.marginTop = `-${third_sec2.offsetHeight}px`;
+}, false);
 window.addEventListener('scroll', flex_header);
 function flex_header(event) {
     let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -52,7 +67,15 @@ function flex_header(event) {
         console.log(windowScroll);
     }
 }
-
+window.addEventListener("orientationchange", function() {
+    menuBtn.style.display = "block";
+    menuCloseBtn.style.display = "none";
+    _header.style.height = `${mobile_logo_block.offsetHeight}px`;
+    if (_header.style.height != height_header) {
+        _header.style.height = height_orient1;
+        first_section.style.margin = margin_orient1;
+    }
+}, false);
 // menu btn
 var i = 0;
 menuBtn.addEventListener('click', () => {
@@ -85,4 +108,18 @@ menuCloseBtn.addEventListener('click', () => {
         
         i++;
     }
-})
+});
+// third
+var z = 0
+third_btn.addEventListener('click', () => {
+    if (z == 0) {
+        third_sec2.style.marginTop = `0px`;
+        third_sign.style.transform = "rotate(180deg)";
+        z++;
+    }
+    else {
+        third_sec2.style.marginTop = `-${third_sec2.offsetHeight}px`;
+        third_sign.style.transform = "rotate(0deg)";
+        z--;
+    }
+});
---
+++ goldofpackard/js/mobile.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    if (this.document.body.offsetWidth < 930) {
+    }
+};
+window.addEventListener("orientationchange", function() {
+    
+}, false);
+window.addEventListener('scroll', function(event) {
+
+});
+window.addEventListener("resize", function() {
+    // Здесь получаем размеры экрана (inner/outerWidth, inner/outerHeight)
+}, false);
+// mobile menu enter
+document.getElementById("mobile_enter").addEventListener(
+    'click', 
+    () => {
+
+    }
+);
+// mobile menu exit
+document.getElementById("mobile_exit").addEventListener(
+    'click', 
+    () => {
+
+    }
+);
